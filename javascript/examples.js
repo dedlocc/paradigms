905e81a8469fc924f4446809440b1987fd6f9251
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2021-03-22 23:20:05 +0300

    JS Objects examples added

--- javascript/examples.js
+++ javascript/examples.js
@@ -10,3 +10,9 @@ include("examples/1_1_types.js");
 include("examples/1_2_functions.js");
 include("examples/1_3_functions-hi.js");
 include("examples/1_4_vectors.js");
+
+// Lecture 2. Objects and Methods
+include("examples/2_1_objects.js");
+include("examples/2_2_closures.js");
+include("examples/2_3_modules.js");
+include("examples/2_4_stacks.js");
